9ce88d3a73cd7e5e843f939f2b3c21dd9fc35c96
damiansolkowicz <damiansolkowicz@outlook.com>
2023-05-09 15:22:04 +0200

    supplies corrections

--- src/main/java/pl/trading/trading/entity/Product.java
+++ src/main/java/pl/trading/trading/entity/Product.java
@@ -25,13 +25,13 @@ public class Product {
     @NotNull
     private Supplier supplier;
 
-    public double getToPay() {
+/*    public double getToPay() {
         return quantity*price;
     }
 
     public void setToPay(double toPay) {
         this.toPay = toPay;
-    }
+    }*/
 /* @ManyToMany
     @JoinTable(
             name = "products_suppliers",
