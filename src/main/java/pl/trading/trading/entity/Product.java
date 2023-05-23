2574a7ba7aa46737a49f3100f7c36a20818dcbb1
damiasolkowicz <damiansolkowicz@outlook.com>
2023-05-23 13:32:39 +0200

    corrections 03

--- src/main/java/pl/trading/trading/entity/Product.java
+++ src/main/java/pl/trading/trading/entity/Product.java
@@ -17,19 +17,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
     private BigDecimal price;
     private BigDecimal quantity;
     private BigDecimal toPay;
     private LocalDate localDate;
     @ManyToOne
     private Unit unit;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @NotNull
     private Supplier supplier;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-
-
 }
